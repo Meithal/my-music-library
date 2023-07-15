0751a2a16757163641582f56bf8a162d4cf4393a
Meithal <hurin8888@gmail.com>
2023-07-16 00:27:40 +0200

    button that opens a fragment

---
+++ app/src/main/java/fr/esgi/students/mymusiclibraryviews/AlbumFragment.kt
@@ -0,0 +1,67 @@
+package fr.esgi.students.mymusiclibraryviews
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.esgi.students.mymusiclibraryviews.placeholder.PlaceholderContent
+
+/**
+ * A fragment representing a list of Items.
+ */
+class AlbumFragment : Fragment() {
+
+    private var columnCount = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyAlbumRecyclerViewAdapter(PlaceholderContent.ITEMS)
+            }
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        "https://www.theaudiodb.com/api/v1/json/2/search.php?s=coldplay"
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            AlbumFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+}
--- app/src/main/java/fr/esgi/students/mymusiclibraryviews/ui/home/HomeFragment.kt
+++ app/src/main/java/fr/esgi/students/mymusiclibraryviews/ui/home/HomeFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.Navigator
+import fr.esgi.students.mymusiclibraryviews.R
 import fr.esgi.students.mymusiclibraryviews.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -32,7 +36,20 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = Bundle()
+        args.putString("artist", "Coldplay");
+
+        val button: Button = binding.button
+        button.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_navigation_home_to_albumFragment) }
+
     }
 
     override fun onDestroyView() {
